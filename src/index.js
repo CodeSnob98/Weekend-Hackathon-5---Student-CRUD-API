42a8a714e1de31eb0d8a7bd726845f1de71cbd76
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-17 17:17:09 +0530

    change3

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ app.put('/api/student/:id', (request, response)=>{
    students[studentIndex].division = updatedStudent.division;
     
     
-    response.send(students[studentIndex]);
+    response.send(updatedStudent);
 });
 
 
